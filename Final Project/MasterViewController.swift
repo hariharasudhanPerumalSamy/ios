70be57ff54b52e3ad5522d720d6b7e050ef366e2
Hariharasudhan <sivam.sudhan@gmail.com>
2017-02-14 03:27:13 +0100

    Minor Changes

--- Final Project/MasterViewController.swift
+++ Final Project/MasterViewController.swift
@@ -18,7 +18,7 @@ extension MasterViewController: changeValueDelegate {
 }
 
 class MasterViewController: UITableViewController {
-
+  
     var detailViewController: DetailViewController? = nil
 
     let model1 = SimpleModel.init(name: "Person1", photo: UIImage(named : "image1"), subtitle: "subtitle1", description: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.")
